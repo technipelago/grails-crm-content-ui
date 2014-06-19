61640b4a428d2cbffeba099ea03d31c1b28cc576
Goran Ehrsson <goran@technipelago.se>
2014-06-19 13:47:01 +0200

    Initial commit

---
+++ web-app/js/crm-ckeditor-config.js
@@ -0,0 +1,20 @@
+CKEDITOR.editorConfig = function( config ) {
+    config.width = '98.3%';
+    config.height = '480px';
+    config.resize_enabled = true;
+    config.startupFocus = true;
+    config.toolbar = [
+        ['Bold', 'Italic', 'Underline', 'Strike'], ['TextColor', 'BGColor'], ['RemoveFormat'],
+        ['Styles', 'Format', 'Font', 'FontSize'],
+        '/',
+        ['Paste', 'PasteText', 'PasteFromWord'],
+        ['JustifyLeft', 'JustifyCenter', 'JustifyRight'],
+        ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent'],
+        ['Image', 'Link', 'Unlink'],
+        ['Table', 'HorizontalRule'],
+        ['Undo', 'Redo'],
+        ['Source']
+    ];
+    config.basicEntities = false;
+    config.protectedSource = [/\[@link\s+[\s\S]*?\[\/@link\]/g, /\[#[\s\S]*?\]/g];
+};
---
+++ web-app/js/crm-ckeditor-styles.js
@@ -0,0 +1,16 @@
+CKEDITOR.stylesSet.add( 'crm-web-styles', [
+    // Block-level styles
+    { name: 'Web: Well', element: 'div', attributes: { 'class': 'well' } },
+    { name: 'Web: Well small', element: 'div', attributes: { 'class': 'well well-small' } },
+    { name: 'Web: Pull Left', element: 'div', attributes: { 'class': 'pull-left' } },
+    { name: 'Web: Pull Right', element: 'div', attributes: { 'class': 'pull-right' } },
+
+    // Inline styles
+    { name: 'Web: Muted', element: 'span', attributes: { 'class': 'muted' } },
+    { name: 'Web: Small', element: 'small' },
+    { name: 'Web: Label', element: 'span', attributes: { 'class': 'label label-info' } },
+    { name: 'Web: Badge', element: 'span', attributes: { 'class': 'badge badge-important' } }
+] );
+
+CKEDITOR.stylesSet.add( 'crm-no-styles', [
+] );
