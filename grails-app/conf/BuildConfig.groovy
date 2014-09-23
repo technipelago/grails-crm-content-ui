075b12dd8db647bb53cc310319573b5f25e76dcc
Goran Ehrsson <goran@technipelago.se>
2014-09-23 13:44:12 +0200

    Upgraded to crm-content 2.0.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,7 +29,7 @@ grails.project.dependency.resolution = {
         test(":codenarc:0.21") { export = false }
         test(":code-coverage:1.2.7") { export = false }
 
-        compile ":crm-content:2.0.0"
+        compile ":crm-content:2.0.3"
         compile ":crm-ui-bootstrap:2.0.0"
 
         compile ":decorator:1.1"
