9eb742a3f8eb796249fa17301e97957240c17900
Goran Ehrsson <goran@technipelago.se>
2015-06-16 09:47:50 +0200

    Updated deps

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -41,7 +41,7 @@ grails.project.dependency.resolution = {
         compile ":ckeditor:4.4.1.0"
         compile ":famfamfam:1.0.1"
 
-        compile ":crm-content:2.4.0"
+        compile ":crm-content:2.4.1"
         compile ":crm-ui-bootstrap:2.4.0"
     }
 }
