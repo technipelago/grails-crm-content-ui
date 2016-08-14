7e9f3c16b7b2297cdd1610b0706cd0322face26c
Goran Ehrsson <goran@technipelago.se>
2016-08-14 21:54:13 +0200

    Version 2.4.2

--- CrmContentUiGrailsPlugin.groovy
+++ CrmContentUiGrailsPlugin.groovy
@@ -18,8 +18,8 @@ import grails.plugins.crm.content.CrmResourceRef
 
 class CrmContentUiGrailsPlugin {
     def groupId = ""
-    def version = "2.4.2-SNAPSHOT"
-    def grailsVersion = "2.2 > *"
+    def version = "2.4.2"
+    def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def loadAfter = ['crmContent']
     def pluginExcludes = [
